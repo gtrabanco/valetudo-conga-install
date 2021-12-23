3a1d273b5323c68c3ac8e7c37a46b4fa5ad56019
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-12-23 22:00:52 +0100

    Do not show messages when checking a connection

--- lib/ssh.sh
+++ lib/ssh.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 test_ssh_connection() {
-  timeout 5 sh -c "</dev/tcp/${1:-}/${2:-22}"
+  timeout 5 sh -c "</dev/tcp/${1:-}/${2:-22}" >/dev/null 2>&1
 }
 
 test_ssh_login() {
-  exec ssh -o BatchMode=yes "$@" "exit 0" || return 1
+  exec ssh -o BatchMode=yes "$@" "exit 0" >/dev/null 2>&1 || return 1
 }
 
 has_ssh_keys() {
